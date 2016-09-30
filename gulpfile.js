29970cff07686e3ec9a122b83bae8f22b3bd7cea
Senya <senya@debian>
2016-09-30 18:18:40 -0400

    Create default gulp-task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var ts = require('gulp-typescript');
+var tsConf = {
+  moduleResolution: 'node',
+  target: 'es5',
+  noImplicitAny: false,
+  rootDir: 'src/'
+};
 
-gulp.task('default', function () {
-  return gulp.src('src/**/*.ts')
-    .pipe(ts({
-      noImplicitAny: false,
-      out: 'output.js'
-    }))
-    .pipe(gulp.dest('built/js'));
+var tsProject = ts.createProject('tsconfig.json', tsConf);
+
+gulp.task('default', ['clean'], function () {
+  return tsProject.src()
+    .pipe(ts(tsProject))
+    .js.pipe(gulp.dest('built'));
+});
+
+gulp.task('clean', function () {
+  return gulp.src('build/', {read: false})
+    .pipe(clean());
 });
