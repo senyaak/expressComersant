4ef00a1e8eef7a7b50ff55a157e2a997def14bd7
Senya <senya@debian>
2016-09-30 14:31:45 -0400

    Build|Remove impicit any

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var ts = require('gulp-typescript');
 gulp.task('default', function () {
   return gulp.src('src/**/*.ts')
     .pipe(ts({
-      noImplicitAny: true,
+      noImplicitAny: false,
       out: 'output.js'
     }))
     .pipe(gulp.dest('built/js'));
