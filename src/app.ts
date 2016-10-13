c0854ce52254ccfd2280c3521d626a8b8777d0d1
Senyaak <senya-arseniy@mail.ru>
2016-10-13 15:04:29 -0400

    Rename template to client

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use((req: express.Request, res: express.Response, next: express.NextFunction
   next();
 });
 app.get('/', (req:express.Request, res:express.Response) => {
-  res.status(200).sendFile(path.resolve(__dirname + '/../src/templates/index.html'));
+  res.status(200).sendFile(path.resolve(__dirname + '/../src/client/index.html'));
 })
 const server = app.listen(conf.port, conf.host, () => { // 3
   //  const {address, port} = server.address(); // 4
