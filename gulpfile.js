a87d4310cfddafaccde50db40407cefdd7ca7b2f
Senyaak <senya-arseniy@mail.ru>
2016-10-14 15:22:22 -0400

    Add gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 var ts = require('gulp-typescript');
+var server = require('gulp-develop-server');
 var tsConf = {
   moduleResolution: 'node',
   target: 'es5',
@@ -14,6 +15,24 @@ gulp.task('default', ['clean'], function () {
   return tsProject.src()
     .pipe(ts(tsProject))
     .js.pipe(gulp.dest('built'));
+});
+
+gulp.task('watch', ['default'], function () {
+  server.listen({path: './built/app.js'});
+  gulp.watch(['./app.js'], server.restart);
+  var watcher = gulp.watch(
+    [
+      'src/**/*.ts'
+    ],
+    {
+      debounceDelay: 1500
+    },
+    ['default']
+  );
+
+  watcher.on('change', function (event) {
+    console.log(`detected change in ${event.path}`);
+  });
 });
 
 gulp.task('clean', function () {
