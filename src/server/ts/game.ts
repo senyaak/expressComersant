e8a7ae144d8ab78b3c25507febcfdac90ad89493
Senyaak <senya-arseniy@mail.ru>
2016-10-14 18:47:06 -0400

    SEVER|Crate game class

---
+++ src/server/ts/game.ts
@@ -0,0 +1,45 @@
+class Game {
+  /* TODO add fields
+    private fields: Field[];
+  */
+  /* TODO add players
+    private players: Player[];
+  */
+  private gameIsRunning: boolean;
+
+  public constructor() {};
+
+  public startGame() {};
+  private nextTurn() {};
+  public isGameRunning(): boolean {
+    return this.gameIsRunning;
+  };
+}
+
+
+/* little help classes */
+
+class Dices {
+  private static dicesCounter: number = 2;
+  public static rollDices(): number {
+    var rolled: number = 0;
+    for(var i = 0;i < Dices.dicesCounter; i++) {
+      rolled += Math.floor(Math.random() * 6);
+    }
+    return rolled;
+  };
+  public static setDicesCounter(newCounter: number): void {
+    if(newCounter > 0) {
+      Dices.dicesCounter = newCounter;
+    } else {
+      throw new Error("New dices counter isn't correct!");
+    }
+  };
+  static
+}
+
+console.log('test');
+var a = 4;
+while(--a > 0) {
+  console.log('Roll:', Dices.rollDices());
+}
