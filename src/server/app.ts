cdc9cf8a595e2231b46692dc3b6d05e960f5dfb2
Senyaak <senya-arseniy@mail.ru>
2016-10-16 13:36:49 -0400

    SERVER|Refactoring app.ts

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,23 +4,28 @@ import * as http from 'http';
 import * as fs from 'fs';
 import * as path from 'path';
 import Game from "./ts/game";
+
+/* get config */
 var conf = JSON.parse(fs.readFileSync('config/config.json', 'utf8'));
 
-var game = new Game();
-game.test();
 
+/* Create server */
 var app = express();
-app.use(express.static("built/client"));
+/* test connections */
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
   console.log('somebody connected');
   next();
 });
+/* routing  */
 app.get('/', (req:express.Request, res:express.Response) => {
   res.status(200).sendFile(path.resolve(__dirname + '/../client/index.html'));
 })
-const server = app.listen(conf.port, conf.host, () => { // 3
-  //  const {address, port} = server.address(); // 4
+app.use(express.static("built/client"));
+app.use('/superagent', express.static("node_modules/superagent"));
+
+/* start server */
+const server = app.listen(conf.port, conf.host, () => {
   var address = server.address().address;
   var port = server.address().port;
-  console.log('Listening on http://localhost:' + port);
+  console.log(`Listening on http://localhost:${port}\nAdress ${address}`);
 });
