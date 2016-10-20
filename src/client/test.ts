96a00d19e648b67442df3c79fc9a23e229169c89
Senyaak <senya-arseniy@mail.ru>
2016-10-20 15:58:53 -0400

    CLIENT|Clear test.ts

--- src/client/test.ts
+++ src/client/test.ts
@@ -1,13 +1,1 @@
 /// <reference path="../../typings/index.d.ts"/>
-
-declare var superagent;
-type Superagent = typeof superagent;
-
-superagent.get('/testRequest').end((err, res) => {
-  if (err) {
-    console.log('error', err);
-  } else {
-    console.log(res);
-  }
-});
-superagent.get();
