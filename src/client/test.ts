ee3fb030d26560cbf4544128b71ebe546089e816
Senyaak <senya-arseniy@mail.ru>
2016-10-22 08:51:39 -0400

    CLIENT|FIELD|Add classes & create field array

--- src/client/test.ts
+++ src/client/test.ts
@@ -1,1 +1,9 @@
 /// <reference path="../../typings/index.d.ts"/>
+/// <reference path="./field.ts"/>
+var socket = io();
+socket.emit('test', 'test value');
+
+socket.on('startGame', (val1,val2,val3) => {
+  field
+  console.log(val1,val2,val3);
+});
