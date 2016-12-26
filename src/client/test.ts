8237ffb1611f2e17225d7b1f9efff9f1b0b60155
Senyaak <senya-arseniy@mail.ru>
2016-12-26 06:08:25 -0500

    CLIENT|Create field

--- src/client/test.ts
+++ src/client/test.ts
@@ -1,9 +1,19 @@
 /// <reference path="../../typings/index.d.ts"/>
 /// <reference path="./field.ts"/>
+
 var socket = io();
 socket.emit('test', 'test value');
 
 socket.on('startGame', (val1,val2,val3) => {
-  field
+  /*create field*/
+  var mainSvg: svgjs.Element = SVG('game').size(
+    9000,//window.innerWidth, 
+    window.innerHeight
+  );
+  var mainGroup = mainSvg.group();
+
+  field.forEach((f,i) => {
+    f.createElement(mainGroup, i);
+  });
   console.log(val1,val2,val3);
 });
